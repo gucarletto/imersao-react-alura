199765b77d1ed59b0d14775e49e232d59c478c34
gucarletto <gucarletto@gmail.com>
2020-07-28 21:53:05 -0300

    Class 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/img/logo.png';
 import './Menu.css';
-//import ButtonLink from './components/ButtonLink';
 import Button from '../Button';
 
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img class='logo' src={logo} alt="gucarlettoflix logo" />
-      </a>
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo Vídeo
       </Button>
     </nav>
