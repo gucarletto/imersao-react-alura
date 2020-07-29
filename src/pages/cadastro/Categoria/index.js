02334a99b7a65807d53f3db230010e9c2b47e018
gucarletto <gucarletto@gmail.com>
2020-07-28 22:05:17 -0300

    Cadastro Categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,6 +7,21 @@ function CadastroCategoria() {
     <PageDefault>
       <h1>Cadastro de categoria</h1>
 
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
       <Link to="/">
         Home
       </Link>
